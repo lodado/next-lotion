f3b08d25840c444c888083012113c800045559e6
lodado <ycp998@naver.com>
2024-10-20 18:51:08 +0900

    feat: logo 구현

--- src/widgets/Navigation/ui/Login/components/LoginNavBar.tsx
+++ src/widgets/Navigation/ui/Login/components/LoginNavBar.tsx
@@ -8,6 +8,7 @@ import { AuthRepositoryImpl, GetUserInfoUseCase } from "@/entities/Auth/core";
 import { LoginButton } from "@/features";
 import { Bell } from "lucide-react";
 import { IconButton } from "@/shared/ui";
+import { Logo } from "../../Logo";
 
 const navLinks = [{ href: "/about", label: "알람" }];
 
@@ -19,10 +20,7 @@ const LoginNavBar = async ({ authRepository }: { authRepository: AuthRepositoryI
       <div className=" mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 px-5">
           <div className="flex items-center flex-shrink-0 flex-row gap-5">
-            <img className="h-8 w-8" src="/logo.svg" alt="Logo" />
-            <span lang="en" className="heading-02">
-              Pokitoki
-            </span>
+            <Logo />
           </div>
           {/* 데스크톱 메뉴 */}
           <div className="block">
