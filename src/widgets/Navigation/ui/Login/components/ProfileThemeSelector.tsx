5e0fd856a95231398d114a734fcad89c5fe36b33
lodado <ycp998@naver.com>
2024-10-20 17:05:42 +0900

    chore: i18n 오타 수정

--- src/widgets/Navigation/ui/Login/components/ProfileThemeSelector.tsx
+++ src/widgets/Navigation/ui/Login/components/ProfileThemeSelector.tsx
@@ -11,45 +11,48 @@ import { Dropdown } from "@/shared/ui";
 
 const ProfileThemeDropdownor = () => {
   const { isMounted, theme, updateTheme, toggleTheme } = useSwitchTheme();
-  const t = useTranslations("ProfileThemeDropdownor");
+  const t = useTranslations("PROFILETHEMESELECTOR");
 
   return (
     <Dropdown>
-      <Dropdown.Trigger variant="custom" className="outline-0 flex flex-col py-2 gap-y-2 w-full border-0 text-inherit">
-        <Dropdown.Item className="flex items-center justify-between w-full px-0 py-2 gap-2">
+      <Dropdown.Trigger
+        variant="custom"
+        className="outline-0 flex flex-col px-0 py-2 gap-y-2 w-full border-0 text-inherit"
+      >
+        <Dropdown.Item className="flex items-center justify-between w-full px-2 py-2 gap-2">
           <span className="flex flex-row items-center gap-2 body-01">
             <SunMoon size={22} />
-            {t("PROFILETHEMEDropdownOR-LABEL-THEME")}
+            {t("LABEL-THEME")}
           </span>
 
           <span className="flex flex-row items-center gap-1 font-extrabold">
-            {t(`PROFILETHEMEDropdownOR-ITEM-${theme.toUpperCase()}`)} <ChevronRight strokeWidth={1.8} size={19} />
+            {t(`ITEM-${theme.toUpperCase()}`)} <ChevronRight strokeWidth={1.8} size={19} />
           </span>
         </Dropdown.Item>
       </Dropdown.Trigger>
-      <Dropdown.Content side="left" align="end" className="mt-[9.2rem] min-w-[10rem]">
+      <Dropdown.Content side="left" align="end" className="mt-[9.6rem] min-w-[10rem]">
         <>
-          <Dropdown.Label>{t("PROFILETHEMEDropdownOR-LABEL-THEME")}</Dropdown.Label>
+          <Dropdown.Label>{t("LABEL-THEME")}</Dropdown.Label>
           <Dropdown.Item
             onClick={() => {
               updateTheme("system");
             }}
           >
-            {t("PROFILETHEMEDropdownOR-ITEM-SYSTEM")}
+            {t("ITEM-SYSTEM")}
           </Dropdown.Item>
           <Dropdown.Item
             onClick={() => {
               updateTheme("dark");
             }}
           >
-            {t("PROFILETHEMEDropdownOR-ITEM-DARK")}
+            {t("ITEM-DARK")}
           </Dropdown.Item>
           <Dropdown.Item
             onClick={() => {
               updateTheme("light");
             }}
           >
-            {t("PROFILETHEMEDropdownOR-ITEM-LIGHT")}
+            {t("ITEM-LIGHT")}
           </Dropdown.Item>
         </>
       </Dropdown.Content>
