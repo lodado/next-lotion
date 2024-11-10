2dee8c1b192679cafc5e39e8fd70c47c84520196
lodado <ycp998@naver.com>
2024-11-10 21:53:21 +0900

    feat: 로고 변경

--- src/widgets/Navigation/ui/Login/components/Logo/Logo.tsx
+++ src/widgets/Navigation/ui/Login/components/Logo/Logo.tsx
@@ -4,18 +4,33 @@ import React from "react";
 import Image from "next/image";
 
 import LogoSvg from "./Logo.svg";
+import { Paytone_One } from "next/font/google";
+
+const paytoneOne = Paytone_One({
+  subsets: ["latin"],
+  weight: "400", // Specify the weight if applicable
+  display: "swap", // Optimize font display
+});
 
 const Logo = () => {
   return (
     <>
       <figure
         lang="en"
-        className="flex w-full h-full flex-row items-center justify-center heading-02 font-extrabold text-color-text-default gap-3"
+        className="flex w-full h-full flex-row items-center justify-center heading-02 font-extrabold text-color-text-default gap-1"
       >
         <Image src={LogoSvg} width={28} height={28} alt="logo" />
-
         <figcaption className="select-none text-color-text-default">
-          Unio
+          <span
+            className={`w-[28px] h-[10px] text-color-text-default ${paytoneOne.className}`}
+            style={{
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }}
+          >
+            unio
+          </span>
           <ScreenReaderOnly>
             is a blogging platform that automatically translates your posts into multiple languages, enabling you to
             effortlessly reach and engage a global audience.
